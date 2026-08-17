7b0d306c07e99fca2a0ee544b30fd75c84667b38
stalpada948-a11y <stalpada948@gmail.com>
2026-08-17 16:22:57 +0530

    Create Wish.js

---
+++ models/Wish.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const wishSchema = new mongoose.Schema({
+    wishId: { type: String, required: true, unique: true },
+    receiverName: { type: String, required: true },
+    senderName: { type: String, required: true },
+    letterText: { type: String, required: true },
+    birthdayDate: { type: String, required: true },
+    images: { type: [String], default: [] }, // Array of image URLs
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Wish', wishSchema);
